fbc9f34a7160e7a7a4cbf8f345ef66a69ae54ce5
Gabe Johnson <gijohnson105@gmail.com>
2016-05-03 13:42:46 -0500

    adding dist/ back

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.toShift = exports.toSpiderMonkey = undefined;
+
+var _toSpidermonkey = require("./to-spidermonkey.js");
+
+var _toSpidermonkey2 = _interopRequireDefault(_toSpidermonkey);
+
+var _toShift = require("./to-shift.js");
+
+var _toShift2 = _interopRequireDefault(_toShift);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.toSpiderMonkey = _toSpidermonkey2.default;
+exports.toShift = _toShift2.default;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7Ozs7QUFDQTs7Ozs7O1FBRVEsYztRQUFnQixPIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHRvU3BpZGVyTW9ua2V5IGZyb20gXCIuL3RvLXNwaWRlcm1vbmtleS5qc1wiO1xuaW1wb3J0IHRvU2hpZnQgZnJvbSBcIi4vdG8tc2hpZnQuanNcIjtcblxuZXhwb3J0IHt0b1NwaWRlck1vbmtleSwgdG9TaGlmdH1cbiJdfQ==
